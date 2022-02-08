f582d9e516e4b50415c88a2060e347fde4218f3b
sf2584 <sf2584@nyu.edu>
2022-02-08 14:46:54 -0500

    add to_rda function in AnnotationTransformer

--- carrada_dataset/utils/transform_annotations.py
+++ carrada_dataset/utils/transform_annotations.py
@@ -75,3 +75,30 @@ class AnnotationTransformer:
             doppler_coordinate += int(self.y_shape/2)
             transformed_annotations.append([range_coordinate, doppler_coordinate])
         return transformed_annotations
+    
+    def to_rda(self):
+        """
+        Method to convert DOA-Doppler points to range-doppler-angle representation
+
+        PARAMETERS
+        ----------
+        None
+
+        RETURNS
+        -------
+        transformed_annotations: list
+             Coordinates in the radar representation
+        """
+        transformed_annotations = list()
+        mid_y_shape = int(self.y_shape/2)
+        for point in self.points:
+            range_value = np.sqrt((point[0] - self.cls.WORLD_RADAR[0])**2 +\
+                                  (point[1] - self.cls.WORLD_RADAR[1])**2)
+            range_coordinate = int(range_value / self.cls.RANGE_RES)
+            angle_value = np.arctan(point[0]/point[1])
+            angle_coordinate = mid_y_shape + int(angle_value / self.cls.ANGLE_RES)
+            doppler_value = point[2]
+            doppler_coordinate = int(doppler_value / self.cls.DOPPLER_RES)
+            doppler_coordinate += int(self.y_shape/2)
+            transformed_annotations.append([range_coordinate, doppler_coordinate, angle_coordinate])
+        return transformed_annotations
